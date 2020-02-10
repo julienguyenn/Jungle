4239adef3f3fbc370102b6ecfe8905fc9c883ed3
Julie Nguyen <julie-nguyen@live.com>
2020-02-10 22:07:41 +0000

    created test for registration and new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
   has_secure_password
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,17 +19,29 @@ RSpec.describe User, type: :model do
       it 'should have a password length of atleast 2' do
       end
     end
+    
+    describe '#first_name' do
+      it 'should be assigned a first name when creating a product' do
+        @user=User.new(first_name:nil, last_name:'nguyen', email: 'julie-nguyen@live.com')
+        expect(@user).to_not be_valid
+      end
+    end
 
-    describe '.authenticate_with_credentials' do
+    describe '#email' do
       it 'cannot create a user with the same email' do
         @user = User.create(:first_name => 'Julie', :last_name => 'Nguyen', :email => 'julie-nguyen@live.com', :password => '123', :password_confirmation => '123')
 
         @user2 = User.create(:first_name => 'Julie', :last_name => 'Nguyen', :email => 'julie-nguyen@live.com', :password => '123', :password_confirmation => '123')
-
-        expect(@user2).to be_nil
+        expect(@user).to be_valid
+        expect(@user2).to_not be_valid
       end
 
       it 'cannot create a user with the same email - not case sensitive' do
+        @user = User.create(:first_name => 'Julie', :last_name => 'Nguyen', :email => 'julie-nGUYEn@live.com', :password => '123', :password_confirmation => '123')
+
+        @user2 = User.create(:first_name => 'Julie', :last_name => 'Nguyen', :email => 'julie-nguyen@live.com', :password => '123', :password_confirmation => '123')
+        
+        expect(@user2).to_not be_valid
       end
     end
   end
