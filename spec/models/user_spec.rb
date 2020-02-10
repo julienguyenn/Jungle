ad5794c7725b8259cdc21a7002d412d43c565dd1
Julie Nguyen <julie-nguyen@live.com>
2020-02-10 20:45:10 +0000

    created tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,10 +18,10 @@ RSpec.describe User, type: :model do
 
     describe '#email' do
       it 'should not create a user with an existing email' do
-        @user1 = User.new(:email => 'julie-nguyen@live.com', :password => '456')
+        @user1 = User.new(:email => 'julie-nguyeN@live.com', :password => '456')
         @user1.save!
         @user2 = User.new(:first_name => 'Julie', :last_name => 'Nguyen', :email => 'julie-nguyeN@live.com', :password => '123', :password_confirmation => '123')
-        expect(User.where(email: 'julie-nguyeN@live.com')).to exist
+        expect(User.where(email: 'julie-nguyen@live.com')).to exist
       end
     end
   end
