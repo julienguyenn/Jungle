2f5426a14776126d090f00d0933c44cb87b01d8d
Julie Nguyen <julie-nguyen@live.com>
2020-02-11 00:15:35 +0000

    added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,16 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  # authenticates the user's email and password
   def self.authenticate_with_credentials(email, password)
+
+    #  removes the spaces in the email input
     email_remove_spaces = email.strip
+
+    # puts it in lowercase to match the database
     email_downcase = email_remove_spaces.downcase
+
+    # finds the email in the database
     user = User.find_by(email: email_downcase)
     if user && user.authenticate(password)
       return user
